9455ef2a7773f56d920abf2b795e31f635791cbc
JPCalsavara <jpcalsavara@gmail.com>
2025-08-20 15:43:57 -0300

    migrations

---
+++ MottuChallenge.API/Migrations/20250818225155_InitialCreate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MottuChallenge.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeliveryPeopleEntities",
+                columns: table => new
+                {
+                    Identifier = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Cnpj = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CnhNumber = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    CnhType = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false),
+                    CnhImageUrl = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeliveryPeopleEntities", x => x.Identifier);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MotorcycleEntities",
+                columns: table => new
+                {
+                    Identifier = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    Model = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    LicensePlate = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MotorcycleEntities", x => x.Identifier);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeliveryPeopleEntities_CnhNumber",
+                table: "DeliveryPeopleEntities",
+                column: "CnhNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeliveryPeopleEntities_Cnpj",
+                table: "DeliveryPeopleEntities",
+                column: "Cnpj",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MotorcycleEntities_LicensePlate",
+                table: "MotorcycleEntities",
+                column: "LicensePlate",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeliveryPeopleEntities");
+
+            migrationBuilder.DropTable(
+                name: "MotorcycleEntities");
+        }
+    }
+}
---
+++ MottuChallenge.API/Migrations/20250819003631_RevertBirthDateToDateTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MottuChallenge.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RevertBirthDateToDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "DeliveryPeopleEntities",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "BirthDate",
+                table: "DeliveryPeopleEntities",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
